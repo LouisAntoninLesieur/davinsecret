c26482f87dd70a3b58b83e1e87f222f7e8df2cfe
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-15 17:00:24 +0100

    feat: build command

--- src/commands/davinsecret.js
+++ src/commands/davinsecret.js
@@ -1,15 +1,16 @@
 import { Command } from "commander";
+import { insertSecret } from "../lib/insertSecret.js";
 
 const program = new Command("davinsecret");
 
 export function davinsecret() {
   program
-    .command("davin <bytes>")
+    .command("secret <bytes>")
+    .alias('s')
     .description('Create and record a JWT Secret to .env file')
     .action((bytes) => {
-      console.log(`davin ${bytes}`);
-    }
-  );
+      const secret = insertSecret(Number(bytes));
+    });
 
   program.parse(process.argv);
 }
