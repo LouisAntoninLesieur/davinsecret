a0795e5de28047181d7f621958cbbfdf09227a12
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-16 13:53:13 +0100

    feat: handle NaN error

--- src/lib/insertSecret.js
+++ src/lib/insertSecret.js
@@ -1,9 +1,18 @@
 import { dotEnvChecker } from "../utils/dotenv-parser/dotEnvChecker.js";
-import { generateSecret } from "../utils/secret-tools/generateRandomHex.js";
-import { secretWriter } from "../utils/secret-tools/secretWriter.js";
+import { generateSecret } from "../utils/generate-secret-tools/generateRandomHex.js";
+import { secretWriter } from "../utils/generate-secret-tools/secretWriter.js";
+import { handleError } from "../utils/handleError.js";
 
 export function insertSecret(bytes) {
-  dotEnvChecker();
-  const secret = generateSecret(bytes);
-  return secretWriter(secret);
+  if (!bytes) {
+    throw new Error(handleError("The type of the provided argument is not a number."));
+  }
+
+  try {
+    dotEnvChecker();
+    const secret = generateSecret(bytes);
+    return secretWriter(secret);
+  } catch (error) {
+    return error;
+  }
 }
