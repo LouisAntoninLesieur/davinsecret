b0d9118b124cebed460a77d9aa9aadd4adae3599
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-18 23:16:49 +0100

    refactor: improve davin documentation

--- src/utils/messages/davinDoc.js
+++ src/utils/messages/davinDoc.js
@@ -15,15 +15,44 @@ What's great about this tool is that it generates a random value for you
 without displaying it in the console. This is a great way to keep your
 secret... secret. 🤫
 --------------------------
-Commands:
+Commands (installed globally): :
 ---------
 command: secret | s
-use    : davinsecret secret <bytes> | s <bytes> : Create and record a JWT Secret to your .env file (or create one if it doesn't exist)
-example: davinsecret secret 32
+use    : davin secret <bytes> | s <bytes> : Create and record a JWT Secret to your .env file (or create one if it doesn't exist)
+example: davin secret 32
 note   : <bytes> MUST BE a number.
 ---------
 command: doc | d
-use    : davinsecret doc | d : Open the documentation
+use    : davin doc | d : Open the documentation
+example: davin doc
+note   : no arguments needed.
+--------------------------
+Commands (using npx):
+---------
+command: secret | s
+use    : npx davinsecret s <bytes> | s <bytes> : Create and record a JWT Secret to your .env file (or create one if it doesn't exist)
+example: npx davinsecret s 32
+note   : <bytes> MUST BE a number.
+---------
+command: doc | d
+use    : npx davinsecret doc | d : Open the documentation
+example: npx davinsecret doc
+note   : no arguments needed.
+--------------------------
+Commands (using npm scripts):
+---------
+command: secret | s
+use    : npm run davin s <bytes> | s <bytes> : Create and record a JWT Secret to your .env file (or create one if it doesn't exist)
+example: npm run davin s 32
+note   : <bytes> MUST BE a number.
+note 2 : Make sure to add the following script to your package.json file :
+        script: {
+          "davin": "davinsecret"
+        }
+          ---------
+command: doc | d
+use    : npm run davin doc | d : Open the documentation
+example: npm run davin doc
 note   : no arguments needed.
 --------------------------
 That's it! Enjoy using Davin's secret generator. 🎉
